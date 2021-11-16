8c07f2803300b16f16cbd30ec88d3ae84c7d6508
dbyoung720 <dbyoung@sina.com>
2021-11-17 07:55:31 +0800

    add note

--- src/db.Image.Rotate.pas
+++ src/db.Image.Rotate.pas
@@ -381,7 +381,7 @@ begin
   rac := Trunc(srac * (1 shl 16));
   ras := Trunc(sras * (1 shl 16));
 
-  { 创建目标图片，耗时 30 毫秒左右 }
+  { 创建目标图片，耗时 20---30 毫秒 }
   bmpDst.PixelFormat        := pf32bit;
   bmpDst.Width              := Round(ABS(bmpSrc.Width * srac) + ABS(bmpSrc.Height * sras));
   bmpDst.Height             := Round(ABS(bmpSrc.Width * sras) + ABS(bmpSrc.Height * srac));
@@ -393,6 +393,7 @@ begin
   CenterX := bmpSrc.Width div 2;
   CenterY := bmpSrc.Height div 2;
 
+  { 旋转图片，耗时 10---20 毫秒 }
   Optimize06(bmpSrc, bmpDst, RotaryAngle, CenterX, CenterY, MoveX, MoveY, ras, rac);
 end;
 
